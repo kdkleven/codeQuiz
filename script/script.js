5e46de541ac14cb461494ab35c700f8a4de011d1
SageTengu <kyle.kleven@outlook.com>
2021-01-11 23:29:56 -0600

    Added timer code and placeholder questions

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
-// Include "View Highscores" link in upper left corner
-// Add "Time: ##" in upper right corner
 // Main content "Coding Quiz Challenge with description"
 // Questions page should include Question, with 4 answers
 // ask 5 questions
@@ -10,7 +8,95 @@
 // then provide a button to either restart quiz or view high scores
 // high scores screen should display user iinitials and their score, with a link to go back to the quiz start screen
 
-var questions = [
-    
-]
 
+// ***DECLARE QUESTIONS***
+var questions = [
+    {
+        question: "Q1",
+        answers: {
+            a: "answer 1",
+            b: "answer 2", 
+            c: "answer 3", 
+            d: "answer 4"
+        }, 
+        correctAnswer: "a"
+    },
+    {
+        question: "Q2",
+        answers: {
+            a: "answer 1",
+            b: "answer 2", 
+            c: "answer 3", 
+            d: "answer 4"
+        }, 
+        correctAnswer: "c"
+    },
+    {
+        question: "Q3",
+        answers: {
+            a: "answer 1",
+            b: "answer 2", 
+            c: "answer 3", 
+            d: "answer 4"
+        }, 
+        correctAnswer: "a"
+    },
+    {
+        question: "Q4",
+        answers: {
+            a: "answer 1",
+            b: "answer 2", 
+            c: "answer 3", 
+            d: "answer 4"
+        }, 
+        correctAnswer: "b"
+    },
+    {
+        question: "Q5",
+        answers: {
+            a: "answer 1",
+            b: "answer 2", 
+            c: "answer 3", 
+            d: "answer 4"
+        }, 
+        correctAnswer: "d"
+    }
+];
+
+// function to build quiz
+var output = [];
+
+for (var i = 0; i < questions.length; i++) {
+    
+}
+
+// function to display result
+
+
+
+
+// function to set timer
+// // assign variable to the time element and set the timer to 60
+var timeEl = document.querySelector(".time");
+var secondsLeft = 60;
+
+// countdown until the timer is 0 or the number of questions has been answered
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+        console.log(secondsLeft);
+
+        // IF QUESTIONS = 0, end game and display remaining timer as score
+
+        // ELSE if timer is 0, end game and display "NO SCORE RECORDED"
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("Time's up");
+            // END GAME
+            // MESSAGE PLAYER
+        }
+    }, 1000);
+}
+
+setTime();
