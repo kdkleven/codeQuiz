3243cf86aab7480165f7eeeacf84092f9bd439f7
Kyle Kleven <kylekleven8@gmail.com>
2021-01-11 21:44:57 -0600

    Declared questions variable

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// Include "View Highscores" link in upper left corner
+// Add "Time: ##" in upper right corner
+// Main content "Coding Quiz Challenge with description"
+// Questions page should include Question, with 4 answers
+// ask 5 questions
+// Correct/incorrect answer displys "Correct/Wrong" with audio (stretch)
+// Incorrect answers decrease timer by 10 seconds
+// when all questoins have been answered OR timer reaches 0, display "All Done" message
+// All done message should display final score, provide a text box for the user's initials (3 char), and a submit score button
+// then provide a button to either restart quiz or view high scores
+// high scores screen should display user iinitials and their score, with a link to go back to the quiz start screen
+
+var questions = [
+    
+]
+
