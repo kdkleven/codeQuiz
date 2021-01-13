3d1f1968f0ac1f5bf523465e2be0eb2cf751b81d
SageTengu <kyle.kleven@outlook.com>
2021-01-12 21:59:02 -0600

    Updated psuedocode to reflect necessary work

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 // Main content "Coding Quiz Challenge with description"
-// Questions page should include Question, with 4 answers
-// ask 5 questions
+// Setup divs for start page, quiz page, result page, and highcore page (call each one at the appropriate time)
+// Questions page should include Question, with 4 answers (questions[0].question, questions[0].answers.a)
+// ask 5 questions ()
 // Correct/incorrect answer displys "Correct/Wrong" with audio (stretch)
 // Incorrect answers decrease timer by 10 seconds
 // when all questoins have been answered OR timer reaches 0, display "All Done" message
@@ -63,20 +64,30 @@ var questions = [
     }
 ];
 
-// function to build quiz
+// 
+var currentQuestion = 0;
 
 
+// function to start game
+// set timer, reset currentQuestion, call loadquestions
 
-for (var i = 0; i < questions.length; i++) {
-    
+function loadQuestion() {
+    // display question and 4 answers
+    // call questions array referencing currentQuestion
 }
 
-// answer buttons
-
-
-// function to display result
+// function to display result page
 var resultEl = document.querySelector("#result");
 
+
+// END GAME function
+// clear question page, load result page
+// display high score button or restart butto
+
+// function for response onclick
+// check if button pressed == questions[].currentquestion.correctAnswer
+// handle correct (load next question and display CORRECT) vs incorrect answer (display incorrect -10s)
+// if seconds < 10, end game set timer to 0
 
 
 // function to set timer
@@ -89,15 +100,14 @@ function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-        console.log(secondsLeft);
+        //console.log(secondsLeft);
 
-        // IF QUESTIONS = 0, end game and display remaining timer as score
-
-        // ELSE if timer is 0, end game and display "NO SCORE RECORDED"
+        // ELSE if timer  0, end game and display "NO SCORE RECORDED"
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
             alert("Time's up");
-            // END GAME
+            //run endgame()
+            
             // MESSAGE PLAYER
         }
     }, 1000);
